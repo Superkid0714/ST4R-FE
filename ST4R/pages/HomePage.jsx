670c4be2e8f3c41bfcc7db77b5acd3e4eb665179
이은서 <leeeunseo1013@gmail.com>
2025-05-19 21:36:33 +0900

    ✨feat: 로그아웃 기능 구현

--- ST4R/pages/HomePage.jsx
+++ ST4R/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { logoutmutation } from '../src/api/auth';
 
 export default function HomePage() {
   const navigate = useNavigate();
   const location = useLocation();
+  const logout = logoutmutation();
 
   useEffect(() => {
     //처음 화면 열렸을 때만 실행하도록 useEffect사용
@@ -22,6 +24,20 @@ export default function HomePage() {
   return (
     <>
       <span className="text-[#FFFFFF]">홈화면</span>
+
+
+      {/* 테스트 로그아웃 버튼 */}
+      <div
+        onClick={()=>{logout.mutate()}}
+        className="absolute left-[12px] right-[12px] bottom-[40px] h-[60px] bg-[#FFBB02] rounded-[10px]"
+      >
+        <div
+          className="absolute left-[20px] top-[18px] text-base font-extrabold font-['Pretendard']"
+        >
+          <span>로그아웃</span>
+        </div>
+      </div>
+
     </>
   );
 }
