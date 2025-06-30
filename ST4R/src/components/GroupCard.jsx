d7cc2ddbd09b5266b00d3f89dbd1d809764a3e83
이은서 <leeeunseo1013@gmail.com>
2025-07-01 03:38:31 +0900

    ♻️ refactor: 모임 목록 조회 사진 크기 및 글씨 넘침 수정

--- ST4R/src/components/GroupCard.jsx
+++ ST4R/src/components/GroupCard.jsx
@@ -20,14 +20,14 @@ export default function GroupCard({ group }) {
     >
       {group.imageUrls && (
         <img
-          className="absolute w-14 sm:w-20 h-auto left-2 top-3 rounded-xl"
+          className="absolute w-14 sm:w-20 sm:h-20 h-14 left-2 top-3 rounded-xl"
           src={group.imageUrls[0]}
         />
       )}
       <div className="absolute left-[74px] sm:left-[100px] top-4 sm:top-6 justify-start text-base sm:text-xl font-normal font-['Pretendard'] leading-normal">
         {group.name}
       </div>
-      <div className="absolute left-[74px] sm:left-[100px] top-11 sm:top-[55px] justify-start text-[#8F8F8F] text-xs sm:text-base font-normal font-['Pretendard'] leading-none">
+      <div className="absolute right-16 left-[74px] sm:left-[100px] top-11 sm:top-[55px] justify-start text-[#8F8F8F] text-xs sm:text-base font-normal font-['Pretendard'] leading-none truncate">
         {`${month}/${day} ${time}, ${group.location?.marker?.roadAddress}`}
       </div>
       <div className="absolute w-14 sm:w-20 h-9 sm:h-14 right-3 top-6 sm:top-6 flex flex-col justify-center bg-[#101010] rounded-3xl ">
