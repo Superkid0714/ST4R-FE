0891c754f0a6e2086d9aaa58c6d46e7a9920b96c
이은서 <leeeunseo1013@gmail.com>
2025-07-11 03:39:43 +0900

    ♻️refactor: 비번 값 반영 오류 해결

--- ST4R/src/components/JoinModal.jsx
+++ ST4R/src/components/JoinModal.jsx
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useRef } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import error2 from '../assets/icons/error2.svg';
 
 // 모달 상세 내용
 export default function JoinModal({ onClose, hasPassword }) {
-  const [passwordInput, setPasswordInput] = useState(null); //사용자가 입력한 비밀번호
+  const passwordRef = useRef('');
+  // const [passwordInput, setPasswordInput] = useState(null); //사용자가 입력한 비밀번호
   const [passworderror, setPasswordError] = useState(false);
   const BASE_URL = 'http://eridanus.econo.mooo.com:8080';
   const { id } = useParams();
+  const navigate = useNavigate();
 
   //모임 참가 요청
-  const postJoin = async () => {
+  const postJoin = async ({password}) => {
     const res = await axios.post(
       `${BASE_URL}/groups/${id}/members`,
-      passwordInput,
+      { password: password.trim() },
       {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
@@ -30,6 +32,7 @@ export default function JoinModal({ onClose, hasPassword }) {
     onSuccess: () => {
       console.log('참가 완료');
       onClose(); // 모달창 닫기
+      navigate(`/groups/${id}/chats`);
     },
     onError: (err) => {
       if (err.response?.status === 403) {
@@ -56,20 +59,24 @@ export default function JoinModal({ onClose, hasPassword }) {
             className="w-full h-14 p-3 focus:outline-none bg-[#2F2F2F] placeholder:text-[#565656] rounded-xl"
             placeholder="여기에 비밀번호를 입력해주세요... "
             onChange={(e) => {
-              setPasswordInput(e.target.value);
+              passwordRef.current = e.target.value;
             }}
-            onFocus={()=>{setPasswordError(false)}}
+            onFocus={() => {
+              setPasswordError(false);
+            }}
           />
           {passworderror ? (
             <div className="flex gap-1 justify-center">
               <img src={error2}></img>
-              <div className="text-[#FF4343] font-light">잘못된 비밀번호에요</div>
+              <div className="text-[#FF4343] font-light">
+                잘못된 비밀번호에요
+              </div>
             </div>
           ) : null}
           <button
             className="w-full h-14 hover:cursor-pointer leading-12 text-center text-black text-lg font-bold bg-[#FFBB02] rounded-xl"
             onClick={() => {
-              useJoinMutation.mutate();
+              useJoinMutation.mutate({password: passwordRef.current});
             }}
           >
             입력완료
@@ -87,7 +94,7 @@ export default function JoinModal({ onClose, hasPassword }) {
           <button
             className="w-full h-14 hover:cursor-pointer leading-12 text-center text-black text-lg font-bold bg-[#FFBB02] rounded-xl"
             onClick={() => {
-              useJoinMutation.mutate();
+              useJoinMutation.mutate({password: passwordRef.current});
             }}
           >
             참가하기
