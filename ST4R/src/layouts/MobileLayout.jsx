c33b5ffddca872ae4426ebf26d464c7dcbcb5ea4
Superkid0714 <kid1412wer@gmail.com>
2025-07-12 18:22:52 +0900

    ♻️ refactor : 글자 수정

--- ST4R/src/layouts/MobileLayout.jsx
+++ ST4R/src/layouts/MobileLayout.jsx
@@ -4,14 +4,14 @@ import MobileNavBar from '../components/mobile/mobileNavBar';
 export default function MobileLayout() {
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
-      {/* 컨텐츠 영역 (스크롤 가능) */}
+      {/* 컨텐츠 영역  */}
       <main className="flex-1 overflow-y-auto pb-16">
         <div className="mx-auto px-3 py-4">
           <Outlet />
         </div>
       </main>
 
-      {/* 하단 네비게이션 바 (고정) */}
+      {/* 하단 네비게이션 바*/}
       <MobileNavBar />
     </div>
   );
