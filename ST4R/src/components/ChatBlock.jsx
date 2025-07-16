eccfd31742d923ebf89420c1c3de2de28ab7d9c0
이은서 <leeeunseo1013@gmail.com>
2025-07-17 07:18:40 +0900

    ✏️chore: 파일 확장자 변경

--- ST4R/src/components/ChatBlock.jsx
+++ ST4R/src/components/ChatBlock.jsx
@@ -2,9 +2,9 @@ export default function ChatBlock({ message, isMe, nickname }) {
   return (
     <>
       {nickname && !isMe && (
-        <div className="flex mx-2 justify-start font-['Pretendard'] text-sm">{nickname}</div>
+        <div className="flex justify-start font-['Pretendard'] text-sm">{nickname}</div>
       )}
-      <div className={`m-2 flex ${isMe ? 'justify-end' : 'justify-start'}`}>
+      <div className={`flex ${isMe ? 'justify-end' : 'justify-start'}`}>
         <div
           className={`max-w-[70%] px-3 py-2 rounded-xl font-['Pretendard'] text-sm break-words whitespace-pre-wrap
           ${isMe ? 'bg-[#2F2F2F]' : 'bg-[#1D1D1D]'}`}
