909408576362363de9be65c8c52a6619a223fd84
이은서 <leeeunseo1013@gmail.com>
2025-07-20 03:33:48 +0900

    ✨feat: 채팅방 내에서 안읽은 사람 수 추가

--- ST4R/src/components/ChatBlock.jsx
+++ ST4R/src/components/ChatBlock.jsx
@@ -6,6 +6,7 @@ export default function ChatBlock({
   nickname,
   imageUrl,
   chattedAt,
+  unreadCount
 }) {
   const localDate = new Date(chattedAt);
   const timeString = localDate.toLocaleTimeString('ko-KR', {
@@ -27,24 +28,38 @@ export default function ChatBlock({
       <div
         className={`flex font-['Pretendard'] ${isMe ? 'justify-end' : 'justify-start'}`}
       >
-        {chattedAt &&
-          isMe && ( 
-            <div className="whitespace-nowrap self-end mr-2 mb-1  text-xs text-gray-400">
-              {timeString}
+        {isMe && (chattedAt ?
+           ( 
+           <div className='flex flex-col'>
+              {unreadCount && (<div className='text-[11px] text-right mr-2 mt-1 text-yellow-300'>{unreadCount}</div>)}
+              <div className="whitespace-nowrap self-end mr-2 text-xs text-gray-400">
+                {timeString}
+              </div>
             </div>
-          )}
+          ) : (<div className="whitespace-nowrap self-end mr-2 mb-1 text-[11px] text-yellow-300">
+                {unreadCount}
+              </div>)) }
+        
+
         <div
           className={`relative max-w-[70%] px-3 py-2 rounded-xl font-['Pretendard'] text-sm break-words whitespace-pre-wrap
     ${isMe ? 'bg-[#2F2F2F]' : 'bg-[#1D1D1D] ml-8'}`}
         >
           {message}
         </div>
-        {chattedAt &&
-          !isMe && ( 
-            <div className="whitespace-nowrap self-end ml-2 mb-1 text-xs text-gray-400">
-              {timeString}
+        {!isMe && (chattedAt ?
+           ( 
+            <div className='flex flex-col'>
+              {unreadCount && (<div className='text-[11px] ml-2 mt-1 text-yellow-300'>{unreadCount}</div>)}
+              <div className="whitespace-nowrap self-end ml-2 text-xs text-gray-400">
+                {timeString}
+              </div>
             </div>
-          )}
+            
+          ):(  <div className="whitespace-nowrap self-end ml-2  mb-1 text-[11px] text-yellow-300">
+                {unreadCount}
+              </div>))}
+          
       </div>
     </>
   );
