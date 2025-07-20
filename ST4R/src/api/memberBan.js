a3fd33a50d09028cb0e99dc7fdc46395b0e80ea7
이은서 <leeeunseo1013@gmail.com>
2025-07-20 17:32:19 +0900

    ✨feat: 모임원 강퇴하기

---
+++ ST4R/src/api/memberBan.js
@@ -0,0 +1,29 @@
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+export const useMemberBan = (teamId) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: async (userId) => {
+      const res = await axios.delete(
+        `http://eridanus.econo.mooo.com:8080/groups/${teamId}/members/${userId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        }
+      );
+      return res.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey : ['members']});
+      navigate(`/groups/${teamId}/members`);
+    },
+    onError: (error) => {
+      console.error('요청 실패', error);
+    },
+  });
+};
