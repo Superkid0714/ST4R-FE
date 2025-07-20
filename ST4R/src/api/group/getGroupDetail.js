349cc10ce5852fc957abc76624d8c1b311e3fbfe
이은서 <leeeunseo1013@gmail.com>
2025-07-21 00:23:55 +0900

    🐛fix: 모임 상세정보 불러오기 토큰 오류 해결

--- ST4R/src/api/group/getGroupDetail.js
+++ ST4R/src/api/group/getGroupDetail.js
@@ -1,21 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useGetGroupDetail= (id) => {
+export const useGetGroupDetail = (id) => {
   const BASE_URL = 'http://eridanus.econo.mooo.com:8080';
+  const token = localStorage.getItem('token');
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
   return useQuery({
     queryKey: ['group', id],
     queryFn: async () => {
-      const res = await axios.get(
-        `${BASE_URL}/groups/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        }
-      );
+      const res = await axios.get(`${BASE_URL}/groups/${id}`, { headers });
       return res.data;
     },
   });
 };
-
